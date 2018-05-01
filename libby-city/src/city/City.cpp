322af5680e9926f8e1d97bf56834ca3eea9ce183
Kevin Aud <kevinaud@gmail.com>
2018-05-01 16:49:55 -0500

    save

---
+++ libby-city/src/city/City.cpp
@@ -0,0 +1,14 @@
+#ifndef CITY_H
+#define CITY_H
+
+using namespace std;
+
+class City {
+    public:
+        City();
+        ~City();
+
+    private:
+};
+
+#endif
---
+++ libby-city/src/config/config.h
@@ -0,0 +1,5 @@
+#include <string>
+
+// replace with absolute path to the root directory of the project
+// on your machine (with a trailing slash)
+std::string project_dir = "/home/kevin/classes/graphics/libby-city/";
