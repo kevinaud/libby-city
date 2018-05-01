f068dfedef520bbdf6f40c6716a0260fc5586961
Kevin Aud <kevinaud@gmail.com>
2018-05-01 16:45:45 -0500

    save

--- libby-city/src/config/config.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-
-// replace with absolute path to the root directory of the project
-// on your machine (with a trailing slash)
-std::string project_dir = "/home/kevin/classes/graphics/libby-city/";
